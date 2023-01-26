0228892dc2b784849f3bc54914afc78f65b8d024
Andrew <fredickandrewdimo@gmail.com>
2023-01-26 19:19:24 +0800

    Set up the tailwindCSS

---
+++ app.js
@@ -0,0 +1,15 @@
+// const word = "example";
+
+// async function searchWord(word) {
+//   try {
+//     const response = await fetch(
+//       `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+//     );
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// searchWord(word);
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        violet: "#A146E7",
+        lightViolet: "#E9D0FA",
+        blackDark: "#2E2E2E",
+        lightBackground: "#F4F4F4",
+        darkGray: "#707070",
+        lightGray: "E8E8E8",
+      },
+    },
+  },
+  plugins: [],
+};
