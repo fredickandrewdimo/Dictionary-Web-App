b16d11db982322492f64f72d42f3d5781b24d3fe
Andrew <fredickandrewdimo@gmail.com>
2023-02-02 23:11:05 +0800

    Added search and retrieve data from the API

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
-// const word = "example";
+// Get the search input element
+const inputEl = document.getElementById("search-input");
 
-// async function searchWord(word) {
-//   try {
-//     const response = await fetch(
-//       `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-//     );
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// searchWord(word);
-
-const word = "keyboard";
-
-async function searchWord() {
+// Fetch data on Dictionary API
+async function searchWord(word) {
   try {
     const response = await fetch(
       `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     );
-
     const data = await response.json();
-    console.log(data);
+    updateDescription(data);
   } catch (e) {
     console.log(e);
   }
 }
 
-// searchWord();
+// Function to Render the Specific Data Needed
+function updateDescription(data) {
+  console.log(data);
+
+  // Get the word
+  const searchedword = data[0].word;
+  document.getElementById("word").innerText = searchedword;
+  console.log(searchedword);
+
+  // Get the phonetic
+  const phonetic = data[0].phonetic;
+  console.log(phonetic);
+
+  // Get the sound
+
+  // Get the nouns and meanings
+
+  // Get the Synonyms
+
+  // Get the verb meaning and example
+
+  // Get the source url
+  const sourceURL = data[0].sourceUrls;
+  console.log(sourceURL);
+}
+
+// Get the search button element and add click event
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", () => {
+  const word = inputEl.value;
+  console.log(word);
+  searchWord(word);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,7 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
-      colors: {
-        violet: "#A146E7",
-        lightViolet: "#E9D0FA",
-        blackDark: "#2E2E2E",
-        lightBackground: "#F4F4F4",
-        darkGray: "#707070",
-        lightGray: "E8E8E8",
-      },
+      colors: {},
       fontFamily: {
         serif: ["Source Serif Pro"],
         tinos: ["Tinos"],
