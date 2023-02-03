1b4c0728c1b2f99bb41c27ccc9d180c07367c661
Andrew <fredickandrewdimo@gmail.com>
2023-02-03 21:31:21 +0800

    Added noun and verb to the DOM

--- app.js
+++ app.js
@@ -16,34 +16,112 @@ async function searchWord(word) {
 
 // Function to Render the Specific Data Needed
 function updateDescription(data) {
-  console.log(data);
-
   // Get the word
   const searchedword = data[0].word;
   document.getElementById("word").innerText = searchedword;
-  console.log(searchedword);
 
   // Get the phonetic
   const phonetic = data[0].phonetic;
-  console.log(phonetic);
+  document.getElementById("phonetic").innerText = phonetic;
 
   // Get the sound
+  const phonetics = data[0].phonetics;
+  phonetics.forEach((phonetic) => {
+    if (phonetic.audio.length > 0) {
+      const audioURL = phonetic.audio;
+      const audioElement = document.getElementById("audio-element");
+      audioElement.src = audioURL;
+
+      document.getElementById("play-btn").addEventListener("click", () => {
+        audioElement.play();
+      });
+      return;
+    }
+  });
+
+  // NOUNS
 
   // Get the nouns and meanings
+  const meanings = data[0].meanings;
 
-  // Get the Synonyms
+  let nounDefinitions;
+  let nounSynonyms;
+
+  meanings.forEach((meaning) => {
+    if (meaning.partOfSpeech === "noun") {
+      nounDefinitions = meaning.definitions;
+      nounSynonyms = meaning.synonyms;
+    }
+  });
+
+  const meaningList = document.getElementById("meaning-list");
+
+  while (meaningList.firstChild) {
+    meaningList.removeChild(meaningList.firstChild);
+  }
+
+  nounDefinitions.forEach((nounDefinition) => {
+    const list = document.createElement("li");
+    list.innerText = nounDefinition.definition;
+    meaningList.appendChild(list);
+  });
+
+  // Get the Synonyms of nouns
+  const synonymsList = document.getElementById("synonyms-list");
+
+  while (synonymsList.firstChild) {
+    synonymsList.removeChild(synonymsList.firstChild);
+  }
+
+  nounSynonyms.forEach((nounSynonym) => {
+    const list = document.createElement("p");
+    list.innerText = nounSynonym;
+    synonymsList.appendChild(list);
+  });
 
   // Get the verb meaning and example
+  let verbDefinitions;
+  let verbDefinition;
+  let verbExample;
+
+  meanings.forEach((meaning) => {
+    if (meaning.partOfSpeech === "verb") {
+      verbDefinitions = meaning.definitions;
+
+      verbDefinition = verbDefinitions[0].definition;
+      verbExample = verbDefinitions[0].example;
+    }
+  });
+
+  const verbMeaningList = document.getElementById("meaning-list-verb");
+
+  while (verbMeaningList.firstChild) {
+    verbMeaningList.removeChild(verbMeaningList.firstChild);
+  }
+
+  const verbList = document.createElement("li");
+  verbList.innerText = verbDefinition;
+  verbMeaningList.appendChild(verbList);
+
+  const verbExampleList = document.createElement("p");
+  verbExampleList.classList.add("text-gray-500");
+  verbExampleList.innerText = `"${verbExample}"`;
+  verbList.appendChild(verbExampleList);
 
   // Get the source url
   const sourceURL = data[0].sourceUrls;
-  console.log(sourceURL);
+  const sourceUrlEL = document.getElementById("source-url");
+  sourceUrlEL.innerText = sourceURL;
+  sourceUrlEL.href = sourceURL;
+  // Get the souce url button
+  document.getElementById("source-url-btn").href = sourceURL;
 }
 
 // Get the search button element and add click event
 const searchBtn = document.getElementById("search-btn");
 searchBtn.addEventListener("click", () => {
   const word = inputEl.value;
-  console.log(word);
+
   searchWord(word);
+  inputEl.value = "";
 });
